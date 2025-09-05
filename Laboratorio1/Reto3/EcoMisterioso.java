a500a7478540876347e520e88649d6005e39f3d8
JuanDiegoRV <juan.rvelasquez@mail.escuelaing.edu.co>
2025-09-04 22:28:46 -0500

    buffer

--- Laboratorio1/Reto3/EcoMisterioso.java
+++ Laboratorio1/Reto3/EcoMisterioso.java
@@ -1,5 +1,26 @@
 package Laboratorio1.Reto3;
 
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
 public class EcoMisterioso {
     
+    public static String invertirConBuffer(String mensaje) {
+        StringBuffer sb = new StringBuffer(mensaje);
+        return sb.reverse().toString();
+    }
+    
+    public static String ecoCompleto(String mensaje) {
+        StringBuffer buffer = new StringBuffer(repetido);
+        return buffer.reverse().toString();
+    }
+    
+    public static void main(String[] args) {
+        String mensaje = "Hola";
+        
+        java.util.function.Function<String, String> ecoLambda = EcoMisterioso::ecoCompleto;
+        
+        String resultado = ecoLambda.apply(mensaje);
+        System.out.println("Eco misterioso: " + resultado);
+    }
 }
