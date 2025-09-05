ee5caf22bdae584b1ddb4d4270d5ab4eca645b58
ignacio14.castillor@gmail.com <ignacio.castillo-r@mail.escuelaing.edu.co>
2025-09-04 23:09:58 -0500

    Parte del estudiante A

--- Laboratorio1/Reto6/MaquinaDeDecisiones.java
+++ Laboratorio1/Reto6/MaquinaDeDecisiones.java
@@ -5,4 +5,32 @@ import java.util.*;
 public class MaquinaDeDecisiones {
     
     private static Map<String, Runnable> comandos = new HashMap<>();
+
+    static {
+        // Comandos Estudiante A
+        comandos.put("SALUDAR", () -> System.out.println("La máquina dice: ¡Saludos, viajero del tiempo y del código!"));
+        comandos.put("DESPEDIR", () -> System.out.println("La máquina dice: Que los bits te acompañen, hasta la próxima misión."));
+        comandos.put("CANTAR", () -> System.out.println("La máquina canta: 01010101"));
+        comandos.put("DANZAR", () -> System.out.println("La máquina gira y emite chispas: Girando en modo fiesta."));
+    }
+
+    public static void ejecutarComando(String comando) {
+        switch (comando.toUpperCase()) {
+            case "SALUDAR":
+            case "DESPEDIR":
+            case "CANTAR":
+            case "DANZAR":
+                comandos.get(comando.toUpperCase()).run();
+                break;
+            default:
+                System.out.println("Comando no reconocido: " + comando);
+        }
+    }
+    
+    public static void main(String[] args) {
+        ejecutarComando("SALUDAR");
+        ejecutarComando("BROMEAR");
+        ejecutarComando("ANALIZAR");
+        ejecutarComando("DANZAR");
+    }
 }
